b224aab02122f1919d2f1f1cd63330d96932bb2c
Michał Słota <irreftw@gmail.com>
2021-08-30 11:52:28 +0200

    added index page

---
+++ src/main/java/guru/spring/recipeassignment/RecipeAssignmentApplication.java
@@ -0,0 +1,13 @@
+package guru.spring.recipeassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/spring/recipeassignment/RecipeAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.spring.recipeassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
