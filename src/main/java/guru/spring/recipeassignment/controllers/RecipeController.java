a06d669ca99cbec2c53034a3473472d1293f198a
Michał Słota <irreftw@gmail.com>
2022-02-11 15:03:46 +0100

    added 404error page and implemented test for it in recipe cotroller

--- src/main/java/guru/spring/recipeassignment/controllers/RecipeController.java
+++ src/main/java/guru/spring/recipeassignment/controllers/RecipeController.java
@@ -1,11 +1,14 @@
 package guru.spring.recipeassignment.controllers;
 
 import guru.spring.recipeassignment.commands.RecipeCommand;
+import guru.spring.recipeassignment.exceptions.NotFoundException;
 import guru.spring.recipeassignment.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Controller
@@ -57,5 +60,18 @@ public class RecipeController {
         recipeService.deleteById(Long.valueOf(id));
 
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(){
+
+        log.error("Handling not found exception");
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("404error");
+
+        return modelAndView;
     }
 }
